ef2cdbbc4808d0f1374f61c49ad0ecab0aac1aec
erun94510 <erun9414@icloud.com>
2023-01-31 00:12:52 +0900

    [Feat] 탭바 추가

---
+++ Bestagram/Bestagram/Views/ContentView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  Bestagram
+//
+//  Created by Donghoon Bae on 2023/01/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            NavigationStack {
+                EmptyView()
+            }.tabItem {
+                Image(systemName: "house")
+            }
+            NavigationStack {
+                EmptyView()
+            }.tabItem {
+                Image(systemName: "magnifyingglass")
+            }
+            NavigationStack {
+                EmptyView()
+            }.tabItem {
+                Image(systemName: "play.square")
+            }
+            NavigationStack {
+                EmptyView()
+            }.tabItem {
+                Image(systemName: "bag")
+            }
+            NavigationStack {
+                EmptyView()
+            }.tabItem {
+                Image(systemName: "person.circle")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
