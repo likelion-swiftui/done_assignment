c1249667a8654d7d3fca769015dee3528a1b99b9
erun94510 <erun9414@icloud.com>
2023-01-31 00:43:31 +0900

    [Add] 피드 컴포넌트 추가

---
+++ Bestagram/Bestagram/Views/Feed/FeedRow.swift
@@ -0,0 +1,20 @@
+//
+//  FeedRow.swift
+//  Bestagram
+//
+//  Created by Donghoon Bae on 2023/01/31.
+//
+
+import SwiftUI
+
+struct FeedRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FeedRow_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedRow()
+    }
+}
