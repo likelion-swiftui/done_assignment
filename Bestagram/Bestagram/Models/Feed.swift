b187caf2637120b7fca9cbf1bebc3a1f6e84b11a
erun94510 <erun9414@icloud.com>
2023-01-31 00:48:34 +0900

    [Feat] 피드 모델 정의

--- Bestagram/Bestagram/Models/Feed.swift
+++ Bestagram/Bestagram/Models/Feed.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct Feed: Identifiable, Hashable {
+    let id = UUID()
+    let userId: String
+    let date: Date
+    let images: [Image]
+    let content: String
+    let likes: Int
+    let comments: Int
+}
